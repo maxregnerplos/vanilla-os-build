0512cc6e9047f2e386abec209b0a031c6b5a5d7b
mirkobrombin <send@mirko.pm>
2022-09-05 17:31:13 +0200

    install only required packages

--- build/remix.sh
+++ build/remix.sh
@@ -9,7 +9,44 @@ add-apt-repository -y --no-update multiverse
 apt update -y
 
 # Install vanilla Gnome desktop and remove Ubuntu session
-apt install -y vanilla-gnome-desktop vanilla-gnome-default-settings
+apt install -y \
+    vanilla-gnome-desktop \
+    vanilla-gnome-default-settings \
+    bluez \
+    bluez-cups \
+    brltty \
+    cups cups-bsd cups-client cups-filters \
+    file-roller \
+    fonts-indic fonts-kacst-one fonts-khmeros-core fonts-lao fonts-liberation \
+        fonts-lklug-sinhala fonts-noto-cjk fonts-sil-abyssinica fonts-sil-padauk \
+        fonts-thai-tlwg fonts-tibetan-machine fonts-ubuntu \
+    fwupd fwupd-signed \
+    gnome-accessibility-themes \
+    gnome-bluetooth \
+    gnome-disk-utility \
+    gnome-initial-setup \
+    gnome-keyring \
+    gnome-software gnome-software-plugin-flatpak gnome-software-plugin-snap \
+    gnome-terminal \
+    laptop-detect \
+    libglib2.0-bin \
+    libnss-mdns \
+    libpam-gnome-keyring \
+    libproxy1-plugin-gsettings libproxy1-plugin-networkmanager \
+    mousetweaks \
+    nautilus nautilus-extension-brasero nautilus-sendto nautilus-share \
+    network-manager-config-connectivity-ubuntu network-manager-openvpn-gnome network-manager-pptp-gnome \
+    plymouth-theme-spinner \
+    policykit-desktop-privileges \
+    ppa-purge \
+    printer-driver-brlaser printer-driver-c2esp printer-driver-foo2zjs \
+        printer-driver-m2300w printer-driver-min12xxw printer-driver-ptouch \
+        printer-driver-pxljr printer-driver-sag-gdi printer-driver-splix \
+    pulseaudio-module-bluetooth \
+    simple-scan \
+    ubuntu-gnome-wallpapers \
+    xdg-utils
+    --no-install-recommends
 apt install -y -f
 apt purge -y ubuntu-desktop ubuntu-session
 
@@ -25,5 +62,5 @@ snap remove --purge firefox
 snap remove --purge snap-store
 
 # Install flatpak and enable Flathub
-apt install -y flatpak gnome-software gnome-software-plugin-flatpak
+apt install -y flatpak
 flatpak remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
