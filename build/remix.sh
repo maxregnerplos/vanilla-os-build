d20da81d680fee5d64c51cdab4d3e78d7e914091
mirkobrombin <send@mirko.pm>
2022-09-05 17:11:44 +0200

    set default wallpaper

--- build/remix.sh
+++ build/remix.sh
@@ -13,6 +13,13 @@ apt install -y vanilla-gnome-desktop vanilla-gnome-default-settings
 apt install -y -f
 apt purge -y ubuntu-desktop ubuntu-session
 
+# Set default wallpaper
+cat > /usr/share/glib-2.0/schemas/90_ubuntuvanillagnome-wallpaper.gschema.override <<EOF
+[org.gnome.desktop.background]
+picture-uri='file:///usr/share/backgrounds/gnome/adwaita-l.jpg'
+EOF
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+
 # Remove pre-installed snap stuff to prevert issues while building
 snap remove --purge firefox
 snap remove --purge snap-store
