92c8155f0f9eb0224f27309db0e095692e08c40a
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-12-18 11:01:31 +0100

    Update remix.sh

--- build/remix.sh
+++ build/remix.sh
@@ -9,7 +9,92 @@ apt install -y \
     capuser \
     expect \
     curl \
-    gpg
+    gpg \
+    git \
+    git-lfs \
+    git-flow \
+    git-extras \
+    git-annex \
+    git-cola \
+    git-crypt \
+    git-gui \
+    gitk \
+    git-review \
+    git-sh \
+    git-subtree \
+    git-tfs \
+    gitk \
+    gitlab \
+    gitlab-runner \
+    gitlab-shell \
+    gitlab-workhorse \
+    gitslave \
+    gitstats \
+    gitup \
+    gitx \
+    git-remote-gcrypt \
+    git-remote-hg \
+    git-remote-mediawiki \
+    git-remote-gcrypt \
+    git-remote-hg \
+    git-remote-mediawiki \
+    git-remote-svn \
+    git-svn \
+    git-svn-dcommit \
+    git-svn-id \
+    git-svn-multi \
+    git-svn-migrate \
+    git-svn-perl \
+    git-svn-rebase \
+    git-svn-rewrite \
+    git-svn-testsvn \
+    git-svn-up \
+    git-svnwrap \
+    git-svnwrap-perl \
+    gitk \
+    gitlab \
+    gitlab-runner \
+    gitlab-shell \
+    gitlab-workhorse \
+    gitslave \
+    gitstats \
+    gitup \
+    gitx \
+    git-remote-gcrypt \
+    git-remote-hg \
+    git-remote-mediawiki \
+    git-remote-gcrypt \
+    git-remote-hg \
+    git-remote-mediawiki \
+    git-remote-svn \
+    git-svn \
+    git-svn-dcommit \
+    git-svn-id \
+    git-svn-multi \
+    git-svn-migrate \
+    git-svn-perl \
+    git-svn-rebase \
+    git-svn-rewrite \
+    git-svn-testsvn \
+    git-svn-up \
+    git-svnwrap \
+    git-svnwrap-perl \
+    gitk \
+    gitlab \
+    gitlab-runner \
+    gitlab-shell \
+    gitlab-workhorse \
+    gitslave \
+    gitstats \
+    gitup \
+    gitx \
+    git-remote-gcrypt \
+    git-remote-hg \
+    git-remote-mediawiki \
+    git-remote-gcrypt \
+    git-remote-hg \
+    git-remote-mediawiki \
+    git-remote-svn \
 
 # Install Vanilla OS PPA
 curl -s --compressed "https://vanilla-os.github.io/ppa/KEY.gpg" | gpg --dearmor | sudo tee /usr/share/keyrings/vanilla-archive-keyring.gpg
@@ -52,10 +137,31 @@ apt install -y \
     pulseaudio-module-bluetooth \
     simple-scan \
     ubuntu-gnome-wallpapers \
+    ubuntu-session \
+    ubuntu-standard \
+    xdg-user-dirs-gtk \
+    xdg-user-dirs \
+    --no-install-recommends
+    libglib2.0-bin \
+    libnss-mdns \
+    libpam-gnome-keyring \
+    libproxy1-plugin-gsettings libproxy1-plugin-networkmanager \
+    mousetweaks \
+    nautilus nautilus-extension-brasero nautilus-sendto nautilus-share \
+    network-manager-config-connectivity-ubuntu network-manager-openvpn-gnome network-manager-pptp-gnome \
+    plymouth-theme-spinner \
+    policykit-desktop-privileges \
+    ppa-purge \
+    printer-driver-brlaser printer-driver-c2esp printer-driver-foo2zjs \
+        printer-driver-m2300w printer-driver-min12xxw printer-driver-ptouch \
+        printer-driver-pxljr printer-driver-sag-gdi printer-driver-splix \
+    pulseaudio-module-bluetooth \
+    simple-scan \
+    ubuntu-gnome-wallpapers \
     xdg-utils \
     --no-install-recommends
 apt install -y -f
-apt purge -y ubuntu-desktop ubuntu-session
+apt purge -y ubuntu-desktop ubuntu-session ubuntu-minimal   #
 
 # Set default wallpaper
 cat > /usr/share/glib-2.0/schemas/90_ubuntuvanillagnome-wallpaper.gschema.override <<EOF
@@ -82,7 +188,6 @@ else
 fi
 
 # Install Vanilla OS Plymouth theme
-apt install -y plymouth-theme-vanilla
-
+apt install -y plymouth-theme-vanilla os-prober plymouth-label plymouth-theme-spinner  
 # Install Vanilla OS distrologo
 apt install -y vanilla-distrologo
